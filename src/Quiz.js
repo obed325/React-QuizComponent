f274c86ffc152dc313d0a80c95a64896c0f7206c
obed325 <obed325@hotmail.com>
2020-09-16 10:22:31 +0200

    module6

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
 import QuizEnd from './QuizEnd.js'
+import {stat} from 'fs';
 
 let quizData = require('./quiz_data.json')
 
@@ -11,17 +12,17 @@ class Quiz extends Component {
     }
     showNextQuestion() {
         this.setState((state) =>{
-            return {quiz_position:state.quiz_position +1}
+            return {quiz_position: state.quiz_position +1}
         })
     }
 
     render(){
-        const isQuizEnd = ((this.state.quiz_position -1)=== quizData.quiz_questions.length)
+        const isQuizEnd = ((this.state.quiz_position - 1)=== quizData.quiz_questions.length)
         return (
             <div>
                
                 {isQuizEnd ? <QuizEnd /> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>}
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
                 
             </div>
         )
